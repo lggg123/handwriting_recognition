3c26beee2f4eee2c8c01700f8ddd9c9ea5b6a228
lggg123 <machinelearner2334@gmail.com>
2022-01-24 12:55:38 -0800

    tensorflow additions to use app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
-from bidict import bidict
+import numpy as np
+
+from flask import (
+    Flask, render_template, request,
+    redirect, url_for, session
+)
 from random import choice
+from bidict import bidict
+from tensorflow import keras
 
 ENCODER = bidict({
     'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
@@ -11,9 +17,13 @@ ENCODER = bidict({
 })
 
 app = Flask(__name__)
+app.secret_key = 'alphabet_quiz'
 
 @app.route('/')
 def index():
+    # we want to clear the session if not it will show the last letter updated
+    # and not the new leter updated
+    session.clear()
     return render_template("index.html")
 
 @app.route('/add-data', methods=['GET'])
@@ -22,25 +32,69 @@ def add_data_get():
     # draw a 'b'
     # B [0, 0, 0...]
     # labels, images
-    letter = choice(list(ENCODER.keys()))
-    return render_template("addData.html", letter=letter)
+    message = session.get('message', '')
+
+    labels = np.load('data/labels.npy')
+    count = {k: 0 for k in ENCODER.keys()}
+    for label in labels:
+        count[label] += 1
+    count = sorted(count.items(), key=lambda x: x[1])
+    letter = count[0][0]
+
+    # letter = choice(list(ENCODER.keys()))
+    return render_template("addData.html", letter=letter, message=message)
 
 @app.route('/add-data', methods=['POST'])
 def add_data_post():
 
-    print(request.form)
+    label = request.form['letter']
+    labels = np.load('data/labels.npy')
+    labels = np.append(labels, label)
+    np.save("data/labels.npy", labels)
 
-    return render_template("addData.html")
+    pixels = request.form['pixels']
+    pixels = pixels.split(',')
+    # pixels we must change into an array and a type of float
+    img = np.array(pixels).astype(float).reshape(1, 50, 50)
+    imgs = np.load('data/images.npy')
+    imgs = np.vstack([imgs, img])
+    np.save("data/images.npy", imgs)
+
+    session['message'] = f'"{label}" added to the training dataset'
+
+    return redirect(url_for('add_data_get'))
 
     
 
 @app.route('/practice', methods=['GET'])
 def practice_get():
-    return render_template("practice.html")
+
+    letter = choice(list(ENCODER.keys()))
+
+    return render_template("practice.html", letter=letter, correct='')
 
 @app.route('/practice', methods=['POST'])
 def practie_post():
-    return render_template("practice.html")
+
+    letter = request.form['letter']
+
+    pixels = request.form['pixels']
+    pixels = pixels.split(',')
+    img = np.array(pixels).astype(float).reshape(1, 50, 50, 1)
+
+    # this loads the model from the model.
+    model = keras.models.load_model('letter.model')
+
+    # this is the letter prediction from the model and adding the training data.
+    pred_letter = np.argmax(model.predict(img), axis=-1)
+    pred_letter = ENCODER.inverse[pred_letter[0]]
+
+    # if the letter you wrote == predicted letter 
+    correct = 'yes' if pred_letter == letter else 'no'
+
+    letter = choice(list(ENCODER.keys()))
+
+    return render_template("practice.html", letter=letter, correct=correct)
 
 if __name__ == '__main__':
     app.run(debug=True)
